370fc1453c53f4f520736a20765508ba125320c8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-12 00:43:43 +0500

    Add Data

--- Contentful/app/page.tsx
+++ Contentful/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './page.css';
 
 async function getBlogs() {
 
-    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/content_types?access_token=${process.env.CONTENTFUL_ACCESS_KEY}`);
+    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=blogPost`);
 
     if (!res.ok) {
         throw new Error('Failed to Fetch Data');
-    }   
+    }
 
     return res.json();
 };
@@ -16,8 +17,15 @@ const page = async () => {
     const blogs = await getBlogs();
 
     return (
-        <>
-        </>
+        <div className="container">
+            <ul>
+                {blogs.items.map((item: any) => (
+                    <li key={item.sys.id}>
+                        {item.fields.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 };
 
