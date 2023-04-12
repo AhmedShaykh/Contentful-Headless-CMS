23bc73c601829009f942af345a3f7385a98c4a74
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-12 23:51:37 +0500

    Add Rich Text

--- Contentful/app/page.tsx
+++ Contentful/app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import './page.css';
 
 async function getBlogs() {
 
-    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=blogPost`);
+    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type?`, { cache: 'no-store' });
 
     if (!res.ok) {
         throw new Error('Failed to Fetch Data');
@@ -17,15 +18,17 @@ const page = async () => {
     const blogs = await getBlogs();
 
     return (
-        <div className="container">
-            <ul>
-                {blogs.items.map((item: any) => (
-                    <li key={item.sys.id}>
+        <>
+            {blogs.items.map((item: any) => (
+                <div key={item.sys.id} className="container">
+                    <div className="title">
                         {item.fields.title}
-                    </li>
-                ))}
-            </ul>
-        </div>
+                    </div>
+                    <div className="box">{documentToReactComponents(item.fields.articleText)}</div>
+                    <div className="author">{item.fields.name}</div>
+                </div>
+            ))}
+        </>
     )
 };
 
