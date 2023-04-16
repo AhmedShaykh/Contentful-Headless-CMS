0099491c6bf546bbda5dd989ea383c592a9cba44
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-16 22:15:45 +0500

    Add Author

--- Contentful/app/page.tsx
+++ Contentful/app/page.tsx
@@ -4,7 +4,7 @@ import './page.css';
 
 async function getBlogs() {
 
-    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type?`, { cache: 'no-cache' });
+    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=blogPost`, { cache: 'no-cache' });
 
     if (!res.ok) {
         throw new Error('Failed to Fetch Data');
@@ -21,21 +21,28 @@ const page = async () => {
         <div>
             {blogs.items.map((item: any) => {
 
-                // const blogImg = blogs.includes.Asset.filter((img: any) => img.sys.id === item.fields.profileImage.sys.id);
-                // console.log(blogImg);
+                const blogImg = blogs.includes.Asset.find((img: any) => img.sys.id === item.fields.image.sys.id);
+
+                const authorData = blogs.includes.Entry.find((author: any) => author.sys.id === item.fields.authorName.sys.id);
 
                 return (
                     <div key={item.sys.id} className="container">
                         <div className="title">
                             {item.fields.title}
                         </div>
+
                         <div className="box">{documentToReactComponents(item.fields.articleText)}</div>
+
                         <div>
                             <img
-                                className='image'
-                                src={blogs.includes.Asset[0].fields.file.url}
+                                className="image"
+                                src={blogImg.fields.file.url}
                                 alt="Image"
                             />
+                        </div>
+
+                        <div className="author">
+                            {authorData.fields.name}
                         </div>
                     </div>
                 );
